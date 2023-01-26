90e8d51fe7835ae0e28c678f0468f72aebf2cb9c
Ahmed Mohiy <amohiy@amohiy.local>
2023-01-27 00:31:34 +0200

    [Database] create AppDatabase class which will be the entry point for this modul

---
+++ PhotographySchoolLessonApp/AppDataBase/AppDataBase/AppDatabase.swift
@@ -0,0 +1,23 @@
+//
+//  AppDatabase.swift
+//  AppDataBase
+//
+//  Created by Ahmed Mohiy on 27/01/2023.
+//
+
+import Foundation
+
+public class AppDatabase {
+    
+    static let `default`: AppDatabase = AppDatabase()
+    
+    public var lessonRepository: LessonRepository {
+        let lessonController = LessonDataController(databaseManager: AppDatabaseManager.default)
+        return LessonRepository(controller: lessonController)
+    }
+    
+    public var downloadRepository: DownloadRepository {
+        let downloadController = DownloadDataController(databaseManager: AppDatabaseManager.default)
+        return DownloadRepository(controller: downloadController)
+    }
+}
