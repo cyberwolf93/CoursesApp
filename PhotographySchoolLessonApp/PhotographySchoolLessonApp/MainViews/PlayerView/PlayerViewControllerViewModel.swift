b680d68a5ef617ce5b6e39d82896777c3e7c5fdd
Ahmed Mohiy <amohiy@amohiy.local>
2023-01-28 15:34:57 +0200

    [App][Player view] play lesson from local url if downloaded otherwise play it remotelly

--- PhotographySchoolLessonApp/PhotographySchoolLessonApp/MainViews/PlayerView/PlayerViewControllerViewModel.swift
+++ PhotographySchoolLessonApp/PhotographySchoolLessonApp/MainViews/PlayerView/PlayerViewControllerViewModel.swift
@@ -12,16 +12,26 @@ import AVKit
 class PlayerViewControllerViewModel {
     
     let lesson: LessonModel
+    let downloadManager: DownloadManager
     
-    init(lesson: LessonModel) {
+    init(lesson: LessonModel, downloadManager: DownloadManager = DownloadManager()) {
         self.lesson = lesson
+        self.downloadManager = downloadManager
     }
     
     func getAvPlayer() -> AVPlayer? {
+        // play locally
+        if let url = URL(string: downloadManager.getLessonLocalUrl(id: lesson.id)) {
+            let newUrl = URL(fileURLWithPath: url.path)
+            return AVPlayer(url: newUrl)
+        }
+        
+        // pLay remotly
         guard let url = URL(string: lesson.videoUrl) else {
             return nil
         }
         
         return AVPlayer(url: url)
+        
     }
 }
