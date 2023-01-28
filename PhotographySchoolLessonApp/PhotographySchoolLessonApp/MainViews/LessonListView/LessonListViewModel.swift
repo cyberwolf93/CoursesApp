46d5e842f342ac91265a4f73daebb405014e8c79
Ahmed Mohiy <amohiy@amohiy.local>
2023-01-28 15:26:53 +0200

    [App][List view] list the cached lesson if there is no internet connection

--- PhotographySchoolLessonApp/PhotographySchoolLessonApp/MainViews/LessonListView/LessonListViewModel.swift
+++ PhotographySchoolLessonApp/PhotographySchoolLessonApp/MainViews/LessonListView/LessonListViewModel.swift
@@ -9,13 +9,40 @@ import Foundation
 import AppModels
 import Combine
 import ClientAPI
+import AppDataBase
+import Reachability
+
 
 class LessonListViewModel: ObservableObject {
     
     @Published var lessonList: [LessonModel] = []
     private var cancellable: [AnyCancellable] = []
+    var appDatabase: AppDatabase = AppDatabase.default
+    
+    func isInternetConnectionExist() -> Bool {
+        guard let reachability = try? Reachability() else {return false}
+        switch (reachability.connection) {
+        case .unavailable, .none:
+            return false
+        default:
+            return true
+        }
+    }
     
     func getLessons() {
+        if self.isInternetConnectionExist() {
+            self.getLessonsRemote()
+            return
+        }
+        
+        self.getLessonsLocal() 
+    }
+    
+    private func getLessonsLocal() {
+        lessonList = self.appDatabase.lessonRepository.getAll()
+    }
+    
+    private func getLessonsRemote() {
         ClienApiController.default.lessonNetworkManager.fetchLessons()
             .receive(on: DispatchQueue.main)
             .sink { completion in
@@ -24,6 +51,7 @@ class LessonListViewModel: ObservableObject {
                 }
             } receiveValue: { [weak self] lessons in
                 self?.lessonList = lessons
+                self?.appDatabase.lessonRepository.add(lessons: lessons)
             }.store(in: &self.cancellable)
     }
 }
